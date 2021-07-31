e507d7b5510a2920f63a0fbcab6e619695d8f9d3
prakashgun <mail.prakash.india@gmail.com>
2021-07-31 16:09:48 +0530

    Added schema

--- src/model/schema.js
+++ src/model/schema.js
@@ -3,6 +3,34 @@ import { appSchema, tableSchema } from '@nozbe/watermelondb'
 export default appSchema({
   version: 1,
   tables: [
-    // We'll add tableSchemas here later
+    tableSchema({
+        name: 'accounts',
+        columns: [
+            {name: 'name', type: 'string'},
+            {name: 'amount', type: 'number'},
+            { name: 'created_at', type: 'number' },
+            { name: 'updated_at', type: 'number' }
+        ]
+    }),
+    tableSchema({
+        name: 'categories',
+        columns: [
+            {name: 'name', type: 'string'},
+            {name: 'icon_name', type: 'string'},
+            {name: 'icon_type', type: 'string'},
+            { name: 'created_at', type: 'number' },
+            { name: 'updated_at', type: 'number' }
+        ]
+    }),
+    tableSchema({
+        name: 'expenses',
+        columns: [
+            {name: 'name', type: 'string'},
+            {name: 'account_id', type: 'string', isIndexed: true},
+            {name: 'category_id', type: 'string', isIndexed: true},
+            { name: 'created_at', type: 'number' },
+            { name: 'updated_at', type: 'number' }
+        ]
+    }),
   ]
 })
